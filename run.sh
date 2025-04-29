9756cea596da9a278012f7c7404ce6b3eb1591b7
Pavan Kumar C Siddegowda <cspavanckm@gmail.com>
2025-04-29 16:37:59 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 echo "Starting FlowFund services..."
 docker run -d -p 8545:8545 psiddegowda/flowfund-hardhat:latest
 docker run -d -p 9090:9090 psiddegowda/flowfund-server:latest
-docker run -d -p 5173:5173 psiddegowda/flowfund-frontend:latest
+docker run -d -p 80:5173 psiddegowda/flowfund-frontend:latest
 echo "FlowFund services started."
